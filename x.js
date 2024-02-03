220cf918a49fee40e5cd4a012ba84c35fa5c3704
amoscicki <82836981+amoscicki@users.noreply.github.com>
2024-02-03 08:58:04 +0000

    Create x.js

---
+++ x.js
@@ -0,0 +1,1 @@
+javascript:document.body.innerHTML += '<div id="mydiv" style="position: fixed; bottom: 0; right: 0; z-index: 1000; background-color: #f00; color: #fff; padding: 10px;">My Div</div>';
